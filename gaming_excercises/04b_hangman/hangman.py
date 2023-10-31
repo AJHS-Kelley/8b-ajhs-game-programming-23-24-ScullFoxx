35c69ce25b1c5795f9f1aa1720e74b22b4f60fc0
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2023-10-31 13:57:58 -0400

    v0.1
    .split

--- gaming_excercises/04b_hangman/hangman.py
+++ gaming_excercises/04b_hangman/hangman.py
@@ -1,1 +1,4 @@
-# Hangman Game by Alexandra Sculley, v0.0
+# Hangman Game by Alexandra Sculley, v0.1
+
+words = 'purple orange red yellow violet blue teal green banana seagreen beige lavender crimson maroon coral cyan lime turquoise fuchisa olive vermillion aquamarine apricot sienna feldgrau smaragdine xanadu amaranth glaucous skobeloff'.split()
+print(words)
