c4bfb5777cc2a6791b063bed5ceed5af87bba75d
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2023-10-31 14:15:23 -0400

    v0.2
    hangman board

--- gaming_excercises/04b_hangman/hangman.py
+++ gaming_excercises/04b_hangman/hangman.py
@@ -1,4 +1,46 @@
-# Hangman Game by Alexandra Sculley, v0.1
+# Hangman Game by Alexandra Sculley, v0.2
 
 words = 'purple orange red yellow violet blue teal green banana seagreen beige lavender crimson maroon coral cyan lime turquoise fuchisa olive vermillion aquamarine apricot sienna feldgrau smaragdine xanadu amaranth glaucous skobeloff'.split()
-print(words)
+
+# VARIABLE_NAMES in ALL-CAPS ARE CONSTANTS AND NOT MEANT TO CHANGE!
+HANGMAN_BOARD = ['''
+        +---+
+            |
+            |
+            |
+         ======= ''', '''
+        +---+
+        O   |
+            |
+            |
+         =======''',''' 
+        +---+
+        O   |
+        |   |
+            |
+         =======''', '''
+        +---+
+        O   |
+       /|   |
+            |
+         =======''', '''
+        +---+
+        O   |
+       /|\  |
+            |
+         =======''', '''
+        +---+
+        O   |
+       /|\  |
+       /    |
+         =======''', '''
+        +---+
+        O   |
+       /|\  |
+       / \  |
+         =======''']
+
+i = 0
+while i < len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i += 1
