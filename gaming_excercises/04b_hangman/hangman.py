a4ce7588aa98b26419784feeab8f0bf2a0d45546
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2023-10-31 14:50:28 -0400

    v0.4
    display board

--- gaming_excercises/04b_hangman/hangman.py
+++ gaming_excercises/04b_hangman/hangman.py
@@ -1,4 +1,4 @@
-# Hangman Game by Alexandra Sculley, v0.3
+# Hangman Game by Alexandra Sculley, v0.4
 import random
 
 words = 'purple orange red yellow violet blue teal green banana seagreen beige lavender crimson maroon coral cyan lime turquoise fuchisa olive vermillion aquamarine apricot sienna feldgrau smaragdine xanadu amaranth glaucous skobeloff'.split()
@@ -47,8 +47,23 @@ def getRandomWord(wordList): # Return a random word from the list.
     #len(listName) is 1 is EXTREMELY COMMON FOR WORKING WITH  LISTS.
     return wordList[wordIndex]
 
-i = 0
-while i < 50:
-    word = getRandomWord(words)
-    print(word)
-    i += 1
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_BOARD[len(missedLetters)])
+    print()
+    print('Missed Letters:', end = ' ')
+    for eachLetter in missedLetters:
+        print(eachLetter, end = ' ')
+    print()
+    # FINISH THURSDAY
+
+
+
+
+
+
+
+# i = 0
+# while i < 50:
+#     word = getRandomWord(words)
+#     print(word)
+#     i += 1
