0823c2b73248c8be382e5fce8cd37c2c251a6059
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2023-11-15 14:42:39 -0500

    v0.3
    implementing fighting mechanic, trying to summon stats for both CPU and player.

--- gaming_excercises/05A_exampleGameFunctions/05A_exampleGameFunctions.py
+++ gaming_excercises/05A_exampleGameFunctions/05A_exampleGameFunctions.py
@@ -1,4 +1,4 @@
-# Fight Game, Alexandra Sculley, v0.1
+ # Fight Game, Alexandra Sculley, v0.3
 import random
 # Make characters
     # Assign them different attack, health, and defense stats
@@ -9,9 +9,16 @@ import random
 # Start off with CPU then implement a 2 PLAYER system.
 
 # FUNCTIONS DEFINED
+strength = 0
+health = 0
+defense = 0
+cpuAttack = False
+playerAttack = False
 
-def getChar(charcter):
-    pass
+def getChar():
+    getNum = random.randint(0, len(characterList) - 1)
+    char = characterList[getNum]
+    return char
 
 def rollDice(numDice, sizeDice):
     numRolled = 0
@@ -19,23 +26,43 @@ def rollDice(numDice, sizeDice):
     while numRolled < numDice:
         roll = random.randint(1, sizeDice)
         sum += roll
-        print(f"Roll: {roll}\n")
-        print(f"Sum: {sum}\n")
+        #print(f"Roll: {roll}\n")
+        #print(f"Sum: {sum}\n")
         numRolled += 1
     return sum
 
 def genStats():
     playerStats = [
+        0, # HELATH
         0, # STRENGTH
-        0, # HEALTH
         0  # DEFENSE
         ]
     i = 0
     while i < len(playerStats):
         playerStats[i] = rollDice(3, 6)
         i += 1
+    if playerStats[0] >= 6:
+        health = 150
+    else:
+        health = 90
+    if playerStats[1] >= 6:
+        strength = 20
+    else:
+        strength = 10
+    if playerStats[2] >= 6:
+        defense = 30
+    else:
+        defense = 10
+    print(f"Your health is {health}")
+    print(f"You do about {strength} damage per hit.")
+    print(f"You have {defense} extra health.")
 
-
+def attackTime():
+    if playerAttack == True:
+        pass
+    elif cpuAttack == True:
+        cpuChar = 1
+        pass
 
 
 # ~~~ BEGINNING OF THE GAME TEXT ~~~
@@ -65,8 +92,14 @@ while picked == False:
         print("Please select a character from the list.")
     else:
         picked = True
-        print(playerCharacter)
+        print(f"You have selected {playerCharacter}")
 
-cpuChar = getChar(character)
+cpuChar = getChar()
+playerStats = genStats()
+cpuStats = genStats()
+print(f"CPU is character {cpuChar}")
+
+
 while True:
- pass
+    playerAttack = True 
+    
