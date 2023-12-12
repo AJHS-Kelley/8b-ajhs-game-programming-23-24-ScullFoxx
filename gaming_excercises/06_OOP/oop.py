9b368f913da9e60a3762c80de3b16cd3bed0cf36
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2023-12-12 14:02:12 -0500

    v0.2
    if-else block age & weight

--- gaming_excercises/06_OOP/oop.py
+++ gaming_excercises/06_OOP/oop.py
@@ -1,4 +1,4 @@
-# Object-Oriented Programming, Alexandra Sculley, v0.1
+# Object-Oriented Programming, Alexandra Sculley, v0.2
 
 class Person: # Use PascalCase for ClassNames
     def __init__(self, name, age, weight): 
@@ -12,4 +12,20 @@ class Person: # Use PascalCase for ClassNames
 
 
 person1 = Person("Lykia",25 ,135)
+person2 = Person("Sylvia", 19, 122)
 print(person1)
+print(person2)
+
+if person1.weight > person2.weight:
+    print(f"{person1.name} weights more than {person2.name}.\n")
+elif person1.weight == person2.weight:
+    print("Each person weighs the same.\n")
+else:
+    print(f"{person2.name} weights more than {person1.name}.\n")
+
+if person1.age > person2.age:
+    print(f"{person1.name} is older than {person2.name}.\n")
+elif person1.age == person2.age:
+    print("Each person is the same age.\n")
+else:
+    print(f"{person2.name} is older than {person1.name}.\n")
