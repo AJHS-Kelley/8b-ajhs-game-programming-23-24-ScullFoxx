9b2e7ffb09447f3d4ee381cf61c854931e36d45b
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2023-12-12 13:51:02 -0500

    v0.1
    Object into string

--- gaming_excercises/06_OOP/oop.py
+++ gaming_excercises/06_OOP/oop.py
@@ -1,4 +1,15 @@
-# Object-Oriented Programming, Alexandra Sculley, v0.0
+# Object-Oriented Programming, Alexandra Sculley, v0.1
 
 class Person: # Use PascalCase for ClassNames
-    
+    def __init__(self, name, age, weight): 
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    # To String Function -- How the object appears as a string.
+    def __str__(self):
+        return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weight} pounds. \n"
+
+
+person1 = Person("Lykia",25 ,135)
+print(person1)
