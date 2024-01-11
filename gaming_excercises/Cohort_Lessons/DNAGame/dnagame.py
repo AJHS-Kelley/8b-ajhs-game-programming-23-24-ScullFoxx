d75b7b668648e66711325933b93ad67fb9561cfd
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-01-11 14:00:27 -0500

    v0.0
    Importing modules and methods

---
+++ gaming_excercises/Cohort_Lessons/DNAGame/dnagame.py
@@ -0,0 +1,8 @@
+# DNA Replication Game, Sculley Alexandra, v0.0
+
+# Import Entire Needed Modules -- Get the whole tool box.
+import time, datetime
+
+# Import Specific Methods -- Get the specific tool.
+from random import choice
+
