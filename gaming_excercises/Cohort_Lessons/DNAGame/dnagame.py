e6f15dc27b95ad47e65a799077f59555c95c997d
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-01-11 14:23:30 -0500

    v0.1
    Added base generation

--- gaming_excercises/Cohort_Lessons/DNAGame/dnagame.py
+++ gaming_excercises/Cohort_Lessons/DNAGame/dnagame.py
@@ -1,8 +1,29 @@
-# DNA Replication Game, Sculley Alexandra, v0.0
+# DNA Replication Game, Sculley Alexandra, v0.1
 
 # Import Entire Needed Modules -- Get the whole tool box.
 import time, datetime
 
 # Import Specific Methods -- Get the specific tool.
 from random import choice
+
+# Store the DNA Bases
+dnaBases = ["A", "T", "C", "G"]
+
+# GAME FUNCTIONS
+def gameIntro() -> None:
+    pass
+
+def genDNA() -> str:
+    basesGenerated = 0
+    basesRequested = int(input("Please enter a positive integer number of bases to generate.\n"))
+    dnaSequence = ""
+
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBases)
+        basesGenerated += 1
+
+    return dnaSequence
+
+dna = genDNA()
+print(dna)
 
