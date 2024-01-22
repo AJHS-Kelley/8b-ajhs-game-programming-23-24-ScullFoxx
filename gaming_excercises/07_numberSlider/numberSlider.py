164f5a76a4a0871de51cad7d94e9bd35069e7852
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-01-22 13:57:43 -0500

    v0.1
    Constant variables

--- gaming_excercises/07_numberSlider/numberSlider.py
+++ gaming_excercises/07_numberSlider/numberSlider.py
@@ -1,1 +1,18 @@
-# Number Slider. Alexandra, Sculley. Based on a project by Al Sweigart, v0.0
+# Number Slider. Alexandra, Sculley. Based on a project by Al Sweigart, v0.1
+
+import sys, random, pygame
+# sys module provides access to system resources (i.e Operating System Commands)
+
+from pygame.locals import *
+# Allows us to call functions from pygame instead of using just the function name instead of module.function()
+# Example: We can use draw() instead of pygame.draw() 
+
+# Constants for Game Board
+BOARDWIDTH = 4 # COLUMNS
+BOARDHEIGHT = 4 # ROWS
+TILESIZE = 80 # MEASURED IN PIXELS
+WINDOWWIDTH = 640 # MEASUED IN PIXELS
+WINDOWHEIGHT = 480 # MEASURED IN PIXELS
+FPS = 30 
+BLANK = None
+
