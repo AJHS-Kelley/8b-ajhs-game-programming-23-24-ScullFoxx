33c2e7c806eba56616017d3905e57d33212a7b86
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-01-26 14:35:51 -0500

    Learning code

---
+++ gaming_excercises/randomGames/pygameTest.py
@@ -0,0 +1,16 @@
+# Messing around trying to learn code.
+
+import pygame, sys, random
+from pygame.locals import *
+
+BOARDWIDTH = 4  # number of columns in the board
+BOARDHEIGHT = 4 # number of rows in the board
+TILESIZE = 80
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+FPS = 30
+BLANK = None
+
+
+XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
+YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
