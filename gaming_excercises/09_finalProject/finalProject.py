7f1500a0fd8bbe2beaa47b5daf6146993f3a20e8
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-03-27 14:48:36 -0400

    caption working

--- gaming_excercises/09_finalProject/finalProject.py
+++ gaming_excercises/09_finalProject/finalProject.py
@@ -1,1 +1,36 @@
 # Final Project, Alexandra Sculley, v0.0
+
+import pygame, sys, random 
+from sys import exit
+
+resolution = 1 # 0 = Low resolution (800, 600), 1 = high resolution (1920, 1080)
+if resolution == 0:
+    x = 800
+    y = 600
+elif resolution == 1:
+    x = 1920
+    y = 1080
+
+screen = pygame.display.set_mode((x, y))
+
+#while True:
+#    for event in pygame.event.get():
+#        if event.type == pygame.QUIT:
+#            pygame.quit()
+#            exit()
+
+pygame.init()
+
+difficulty = int(input("Please select a difficulty. Enter 1 for Short Dungeon and 2 for Long Dungeon\n"))
+
+if difficulty == 1:
+    z = "Dungeon Explorer: Short dungeon"
+else:
+    z = "Dungeon Explorer: Long dungeon"
+
+pygame.display.set_caption(z)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
