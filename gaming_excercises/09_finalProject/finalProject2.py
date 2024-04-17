9a3e4ea7732c934ad7d4efd965b9c0675a504e33
ScullFoxx <142345245+ScullFoxx@users.noreply.github.com>
2024-04-17 14:11:14 -0400

    Movement update complete

--- gaming_excercises/09_finalProject/finalProject2.py
+++ gaming_excercises/09_finalProject/finalProject2.py
@@ -3,14 +3,13 @@
 import pygame, sys, random 
 from sys import exit
 clock = pygame.time.Clock()
-x = 800
-y = 600
-xButOp1, xButOp2, xButOp3 = 180, 70, 290
-yButOp1, yButOp2 = 430, 540
-xSlotOp1, xSlotOp2, xSlotOp3 = 740, 628, 516
-gameScreenX, gameScreenY = 200, 370
-playerX, playerY = 300, 300
-leftCeiling, rightCeiling, floorCeiling, topCeiling =  250, 445, 259, 42
+x = 1920
+y = 1080
+xButOp1, xButOp2, xButOp3 = 260, 100, 420
+yButOp1, yButOp2 = 830, 990
+xSlotOp1, xSlotOp2, xSlotOp3 = 1760, 1600, 1440
+gameScreenX, gameScreenY = 600, 730
+leftCeiling, rightCeiling, floorCeiling, topCeiling =  700, 1060, 478, 78
 
 screen = pygame.display.set_mode((x, y))
 
@@ -20,31 +19,63 @@ class InventorySlots: # Use PascalCase for ClassNames
         self.yValue = yValue
         self.Image = pygame.image.load('Img/slot.png')
         self.Scale = None
-slot1 = InventorySlots(xSlotOp1, yButOp2)
-slot2 = InventorySlots(xSlotOp2, yButOp1)
-slot3 = InventorySlots(xSlotOp2, yButOp1)
-slot4 = InventorySlots(xSlotOp2, yButOp2)
-slot5 = InventorySlots(xSlotOp3, yButOp2)
-slot6 = InventorySlots(xSlotOp3, yButOp1)
 
-vel = 1
+# Surfaces
+slot1 = InventorySlots(xSlotOp1, 760)
+slot2 = InventorySlots(xSlotOp1, 920)
+slot3 = InventorySlots(xSlotOp2, 920)
+slot4 = InventorySlots(xSlotOp2, 760)
+slot5 = InventorySlots(xSlotOp3, 760)
+slot6 = InventorySlots(xSlotOp3, 920)
+buttonUPSurf = pygame.image.load('img/buttonUp.png')
+buttonDOWNSurf = pygame.image.load('img/buttonDown.png')
+buttonLEFTSurf = pygame.image.load('img/buttonLeft.png')
+buttonRIGHTSurf = pygame.image.load('img/buttonRight.png')
+gameScreen = pygame.image.load('img/DungeonRoom.png')
+
+# Player
+playerSurf = pygame.image.load('img/slot.png')
+playerX, playerY = 900, 200
+
+# Rectangles
+gameRect = gameScreen.get_rect(bottomleft = (gameScreenX, gameScreenY))
+buttonUPRect = buttonUPSurf.get_rect(center = (xButOp1, yButOp1))
+buttonDOWNRect = buttonDOWNSurf.get_rect(center = (xButOp1, yButOp2))
+buttonLEFTRect = buttonLEFTSurf.get_rect(center = (xButOp2, yButOp2))
+buttonRIGHTRect = buttonRIGHTSurf.get_rect(center = (xButOp3, yButOp2))
+
+vel = 3
 run = True
 while run:
-    clock.tick(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
-    #if keys[pygame.K_w]:
-    #    if playerY > 10:
-    #        playerY -= vel
-    #if keys[pygame.K_d]:
-    #    playerX += vel
-    #if keys[pygame.K_s]:
-    #    playerY += vel
-    #if keys[pygame.K_a]:
-    #    playerX -= vel
-    screen.blit(slot1, (slot1.xValue, slot1.yValue))
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        if playerY > topCeiling:
+            playerY -= vel
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        if playerX < rightCeiling:
+            playerX += vel
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        if playerY < floorCeiling:
+            playerY += vel
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        if playerX > leftCeiling:
+            playerX -= vel
+
+    screen.blit(slot1.Image, (slot1.xValue, slot1.yValue))
+    screen.blit(slot2.Image, (slot2.xValue, slot2.yValue))
+    screen.blit(slot3.Image, (slot3.xValue, slot3.yValue))
+    screen.blit(slot4.Image, (slot4.xValue, slot4.yValue))
+    screen.blit(slot5.Image, (slot5.xValue, slot5.yValue))
+    screen.blit(slot6.Image, (slot6.xValue, slot6.yValue))
+    screen.blit(buttonRIGHTSurf, buttonRIGHTRect)
+    screen.blit(buttonUPSurf, buttonUPRect)
+    screen.blit(buttonLEFTSurf, buttonLEFTRect)
+    screen.blit(buttonDOWNSurf, buttonDOWNRect)
+    screen.blit(gameScreen, gameRect)
+    screen.blit(playerSurf, (playerX, playerY))
     #window.fill((0, 0, 0))
     pygame.display.update()
